497d22b1fe85445a34c12a4469b2fd02fc0c4f4d
ydai713 <yang.dai@klaviyo.com>
2024-11-16 14:24:10 -0800

    re-org data files into ddl and single data file

---
+++ ddl/create_transaction_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE transactions
+(
+    chain_id         integer NOT NULL,
+    block_number     integer,
+    block_timestamp  timestamp WITH TIME ZONE,
+    "from"           text,
+    "to"             text,
+    transaction_hash text    NOT NULL,
+    value            numeric,
+    PRIMARY KEY (transaction_hash, chain_id)
+);
